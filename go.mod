c93038046d1cd7b81bc2f29728ad46f28ef9f387
Murat Tunc <murat.tunc@siemens.com>
2024-01-10 15:20:53 +0300

    func main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoDataStructers
+
+go 1.21.5
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
+package main
+
+import "fmt"
 
 // BinarySearch function
 func BinarySearch(a []int, x int) int {
     result := -1 // not found
     start := 0
     end := len(a) - 1
+
     for start <= end {
         mid := (start + end) / 2
         if a[mid] == x {
@@ -15,5 +19,15 @@ func BinarySearch(a []int, x int) int {
             end = mid - 1
         }
     }
+
     return result
+}
+
+
+func main() {
+
+    array := []int{10,9,7,2,11,3,65,89,99}
+    search_value:=11
+    fmt.Println(BinarySearch(array,search_value))
+    
 }
