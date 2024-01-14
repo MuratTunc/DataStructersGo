ef864f08cb87d5d01e2a8f05eb08abfb47cfe789
Murat Tunc <murat.tunc@siemens.com>
2024-01-14 23:48:11 +0300

    Added buble sort function!

--- main.go
+++ main.go
@@ -23,11 +23,37 @@ func BinarySearch(a []int, x int) int {
     return result
 }
 
+func bubbleSort(arr []int) {
+	n := len(arr)
+	swapped := true
+	m := 0
+
+	for swapped {
+		swapped = false
+		
+		fmt.Println("iteration number: " ,m)
+		for i := 1; i < n; i++ {
+			if arr[i-1] > arr[i] {
+				// Swap elements if they are in the wrong order
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+				swapped = true
+			}
+		}
+		n-- // The largest element is now in its correct position, 
+            // so reduce the length of the unsorted part of the array
+		
+		fmt.Println("Sorted array:", arr)
+		m++
+	}
+}
 
 func main() {
 
     array := []int{10,9,7,2,11,3,65,89,99}
     search_value:=11
     fmt.Println(BinarySearch(array,search_value))
+
+    bubbleSort(array)
+	fmt.Println("The sorted array is:", array)
     
 }
