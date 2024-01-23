51edf5f3f83e22d710d725b5fb3ca1a872cbf233
Murat Tunc <murat.tunc@siemens.com>
2024-01-23 16:34:12 +0300

    Added interpolationSearch !

--- main.go
+++ main.go
@@ -58,6 +58,52 @@ func bubbleSort(arr []int) {
 	}
 }
 
+//interpolationSearch
+func interpolationSearch(array []int, key int) int {
+
+	min, max := array[0], array[len(array)-1]
+
+	low, high := 0, len(array)-1
+
+	for {
+		if key < min {
+			return low
+		}
+
+		if key > max {
+			return high + 1
+		}
+
+		// make a guess of the location
+		var guess int
+		if high == low {
+			guess = high
+		} else {
+			size := high - low
+			offset := int(float64(size-1) * (float64(key-min) / float64(max-min)))
+			guess = low + offset
+		}
+
+		// maybe we found it?
+		if array[guess] == key {
+			// scan backwards for start of value range
+			for guess > 0 && array[guess-1] == key {
+				guess--
+			}
+			return guess
+		}
+
+		// if we guessed to high, guess lower or vice versa
+		if array[guess] > key {
+			high = guess - 1
+			max = array[high]
+		} else {
+			low = guess + 1
+			min = array[low]
+		}
+	}
+}
+
 // Function to search x in the given array
 func SentinelSearch(arr []int, key int) int {
     result := -1 // not found
@@ -85,6 +131,9 @@ func main() {
 
     array := []int{10,9,7,2,11,3,65,89,99}
     search_value:=11
+
+	array2 := []int{1,2, 9, 20, 31, 45, 63, 70, 100}
+	fmt.Println(interpolationSearch(array2,45))
 
 	fmt.Println("array:",array)
 	fmt.Println("search_value:",search_value)
